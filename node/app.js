a214d12962e48417ca4f0913ee0e0e70f0f6c15d
Anastase Tuyizere <42033331+TuyizeeAnastase@users.noreply.github.com>
2023-03-16 11:23:23 +0200

    pdf-reader

---
+++ node/app.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const pdfParse = require('pdf-parse')
+const getPDF = async (file) => {
+  let readFileSync = fs.readFileSync(file)
+  try {
+    let pdfExtract = await pdfParse(readFileSync)
+    console.log('File content: ', pdfExtract.text)
+    console.log('Total pages: ', pdfExtract.numpages)
+    console.log('All content: ', pdfExtract.info)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+const pdfRead = '../example.pdf'
+getPDF(pdfRead)
